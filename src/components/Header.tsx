54fd639a9c05b9479e45b4b58b5d8af72c04ff90
Trần Đức Huy <118761956+duchuyash@users.noreply.github.com>
2026-03-10 13:00:55 +0700

    ui header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <nav className='navbar navbar-expand-lg p-2 navbar-light bg-white fixed-top'>
+            <a className='navbar-brand text-muted font-weight-bold' href='#'>
+                Header
+            </a>
+        </nav>
+    );
+};
+
+export default Header;
